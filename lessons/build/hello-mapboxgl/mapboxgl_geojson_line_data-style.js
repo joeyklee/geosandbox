74b70700fb14951e78fae6b9d098d08a1e8cc11a
joeyklee <jsphknglee@gmail.com>
2019-10-22 21:19:40 -0400

    updates examples

---
+++ lessons/build/hello-mapboxgl/mapboxgl_geojson_line_data-style.js
@@ -0,0 +1,63 @@
+initMap();
+
+map.on("load", function () {
+
+
+  const myLine = turf.lineString([
+    [
+      -122.26100921630858,
+      37.821446448926444
+    ],
+    [
+      -122.22770690917969,
+      37.79730575499309
+    ],
+    [
+      -122.25654602050781,
+      37.766643840752764
+    ],
+    [
+      -122.3262405395508,
+      37.790523241426946
+    ],
+    [
+      -122.2671890258789,
+      37.86645181975609
+    ],
+    [
+      -122.18582153320312,
+      37.821446448926444
+    ],
+    [
+      -122.21157073974608,
+      37.778584505321376
+    ]
+  ], {
+    name: 'The best line in Oakland.',
+    myColor: 'blue',
+    myWidth: 10
+  });
+
+
+
+  // Add a single point to the map
+  map.addSource('myLine', {
+    "type": "geojson",
+    "data": myLine
+  });
+
+  map.addLayer({
+    "id": "myLine",
+    "type": "line",
+    "source": "myLine",
+    'layout': {
+      'line-cap': 'round',
+      'line-join': 'round'
+      },
+    'paint': {
+      'line-color': ['get', 'myColor'],
+      'line-width': ['get', 'myWidth'],
+      'line-opacity': .8
+      }
+  });
+})
