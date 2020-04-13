c7e335e5f30688a41e08fdfa7380b808b9baf41e
joeyklee <joeyklee@nyu.edu>
2020-04-13 14:23:52 -0400

    updates deckgl scatter layer example

--- lessons/src/hello-deckgl/deckgl_scatterplot_layer.js
+++ lessons/src/hello-deckgl/deckgl_scatterplot_layer.js
@@ -1,36 +1,33 @@
 initMap();
 
-const data = [
-  {coordinates:[0,0]}
-];
+map.on("load", () => {
 
-const scatterplot = new deck.ScatterplotLayer({
-  id: 'scatterplot-layer',
-  data,
-  pickable: true,
-  opacity: 1,
-  stroked: true,
-  filled: true,
-  radiusScale: 6,
-  radiusMinPixels: 1,
-  radiusMaxPixels: 100,
-  lineWidthMinPixels: 1,
-  getPosition: d => d.coordinates,
-  getRadius: d => Math.sqrt(1000000),
-  getFillColor: d => [255, 140, 0],
-  getLineColor: d => [0, 0, 0],
-  onHover: ({object, x, y}) => {
-    const tooltip = `null island`;
-    /* Update tooltip
-      http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
-    */
-  }
-})
+  const myPoints = [
+    { coordinates: [0.45, 51.47], rad:10000},
+    { coordinates: [0, 0],rad:10000 },
+    { coordinates: [-123.1, 49.2],rad:10000 },
+  ];
 
-const layers = [
-  scatterplot
-]
+  app.setProps({
+    layers: [
+      new deck.ScatterplotLayer({
+        id: "my-scatterplot",
+        data: myPoints,
+        pickable: true,
+        autoHighlight: true,
+        opacity: 1,
+        stroked: true,
+        filled: true,
+        radiusScale: 6,
+        radiusMinPixels: 1,
+        radiusMaxPixels: 100,
+        lineWidthMinPixels: 1,
+        getPosition: (d) => d.coordinates,
+        getRadius: (d) => d.rad,
+        getFillColor: (d) => [255, 140, 0],
+        getLineColor: (d) => [0, 0, 0],
+      }),
+    ],
+  });
 
-app.setProps({layers});
-
-// app.setProps([layer]);
+});
--- lessons/src/hello-deckgl/init_map.js
+++ lessons/src/hello-deckgl/init_map.js
@@ -1,45 +1,44 @@
 let map;
 let app;
 
-const INITIAL_VIEW_STATE = {
-  latitude: 51.47,
-  longitude: 0.45,
-  zoom: 4,
-  bearing: 0,
-  pitch: 30
-};
+function initMap() {
+  const INITIAL_VIEW_STATE = {
+    latitude: 51.47,
+    longitude: 0.45,
+    zoom: 4,
+    bearing: 0,
+    pitch: 30,
+  };
 
-// Set your mapbox token here
-mapboxgl.accessToken = "pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw";
+  // Set your mapbox token here
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw";
 
-function initMap(){
-    map = new mapboxgl.Map({
-    container: 'mymap',
-    style: 'mapbox://styles/mapbox/light-v9',
+  map = new mapboxgl.Map({
+    container: "mymap",
+    style: "mapbox://styles/mapbox/light-v9",
     // Note: deck.gl will be in charge of interaction and event handling
     interactive: true,
     center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
     zoom: INITIAL_VIEW_STATE.zoom,
     bearing: INITIAL_VIEW_STATE.bearing,
-    pitch: INITIAL_VIEW_STATE.pitch
-    });
+    pitch: INITIAL_VIEW_STATE.pitch,
+  });
 
-    app = new deck.Deck({
-    canvas: 'deck-canvas',
-    width: '100%',
-    height: '100%',
+  app = new deck.Deck({
+    canvas: "deck-canvas",
+    width: "100%",
+    height: "100%",
     initialViewState: INITIAL_VIEW_STATE,
     controller: true,
-    onViewStateChange: ({viewState}) => {
-        map.jumpTo({
+    onViewStateChange: ({ viewState }) => {
+      map.jumpTo({
         center: [viewState.longitude, viewState.latitude],
         zoom: viewState.zoom,
         bearing: viewState.bearing,
-        pitch: viewState.pitch
-        });
+        pitch: viewState.pitch,
+      });
     },
-    layers: []
-    });
+  });
 }
-
 initMap();
