57edc8d9e619f9fbb7e1c974d17991c9c530da87
joeyklee <joeyklee@nyu.edu>
2020-04-27 14:59:35 -0400

    adds examples

---
+++ lessons/src/hello-d3geo/style_basemap.js
@@ -0,0 +1,42 @@
+let svg;
+let basemap;
+
+async function init() {
+  const width = 400,
+    height = 400;
+
+  const basemapURL =
+    "https://gist.githubusercontent.com/joeyklee/2faa602425bcb9c8bbc1a2319190051d/raw/8bdf7afce45fd282b8859a9efe1edffb7a3956ea/nyc_borough_bounds.geojson";
+
+  basemap = await fetch(basemapURL);
+  basemap = await basemap.json();
+
+  svg = d3
+    .select("#mymap")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background-color", "#eee");
+
+  const projection = d3
+    .geoAlbers()
+    .center([0, 40.714666])
+    .rotate([73.947008, 0])
+    .scale(40000)
+    .translate([width / 2, height / 2]);
+
+  const geoPath = d3.geoPath().projection(projection);
+
+  svg
+    .append("g")
+    .selectAll("path")
+    .data(basemap.features)
+    .enter()
+    .append("path")
+    .attr("d", geoPath)
+    .attr("class", "borough")
+    .style("fill", "#CCCCCC")
+    .style("stroke", "#FFFFFF")
+    .style("stroke-width", "1.2px");
+}
+init();
